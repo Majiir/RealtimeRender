fd69d51484549614d5770645bed62b238dcd705b
Majiir <henry@zephyrhosting.net>
2011-12-04 20:28:30 -0500

    ImageHeightMap extends HeightMapChunkProvider instead of directly implementing HeightMap.

--- RealtimeRender/src/main/java/net/nabaal/majiir/realtimerender/rendering/ImageHeightMap.java
+++ RealtimeRender/src/main/java/net/nabaal/majiir/realtimerender/rendering/ImageHeightMap.java
@@ -7,42 +7,38 @@ import java.awt.image.BufferedImage;
 import net.nabaal.majiir.realtimerender.Coordinate;
 import net.nabaal.majiir.realtimerender.image.ImageProvider;
 
-public class ImageHeightMap implements HeightMap {
+public class ImageHeightMap extends HeightMapChunkProvider {
 
 	private final ImageProvider imageProvider;
 	
 	public ImageHeightMap(ImageProvider imageProvider) {
 		this.imageProvider = imageProvider;
 	}
-
+	
 	@Override
-	public int getHeight(Coordinate point) {
-		Color color = imageProvider.getPixelColor(point, Coordinate.OFFSET_BLOCK_CHUNK);
-		int height = color.getBlue();
-		if ((height > 127) || (color.getAlpha() == 0)) { // TODO: Better constants and valid-height detection
-			return NO_HEIGHT_INFORMATION;
-		}
-		return height;
-	}
-
-	@Override
-	public void setHeight(Coordinate point, int height) {
+	protected HeightMapChunk getHeightMapChunk(Coordinate chunkLocation) {
 		
-		// TODO: Make not retarded
-		throw new UnsupportedOperationException();
-		/*
-		Color color;
-		if (height == NO_HEIGHT_INFORMATION) {
-			color = new Color(0, 0, 128);
-		} else {
-			color = new Color(0, 0, height);
+		BufferedImage image = imageProvider.getImage(chunkLocation);
+		HeightMapChunk chunk = new HeightMapChunk(chunkLocation);
+		
+		// TODO: Enumerate over coordinates
+		for (int x = 0; x < 16; x++) {
+			for (int y = 0; y < 16; y++) {
+				Color color = new Color(image.getRGB(x, y));
+				int height = color.getBlue();
+				if (height > 127) { // TODO: Better constants and valid-height detection
+					height = NO_HEIGHT_INFORMATION;
+				}
+				chunk.setHeight(chunkLocation.zoomIn(Coordinate.OFFSET_BLOCK_CHUNK).plus(new Coordinate(x, y, Coordinate.LEVEL_BLOCK)), height);
+			}
 		}
-
-		imageProvider.setPixelColor(point, color, Coordinate.OFFSET_BLOCK_CHUNK);
-		*/
+		
+		return chunk;
+		
 	}
 	
-	public void setChunk(Coordinate chunk, HeightMapChunk hmc) {
+	@Override
+	protected void setHeightMapChunk(Coordinate chunkLocation, HeightMapChunk chunk) {
 		
 		BufferedImage image = ImageProvider.createImage(Coordinate.SIZE_CHUNK);
 		Graphics2D graphics = image.createGraphics();
@@ -51,7 +47,7 @@ public class ImageHeightMap implements HeightMap {
 		for (int x = 0; x < 16; x++) {
 			for (int y = 0; y < 16; y++) {
 				
-				int height = hmc.getHeight(chunk.zoomIn(Coordinate.OFFSET_BLOCK_CHUNK).plus(new Coordinate(x, y, Coordinate.LEVEL_BLOCK)));
+				int height = chunk.getHeight(chunkLocation.zoomIn(Coordinate.OFFSET_BLOCK_CHUNK).plus(new Coordinate(x, y, Coordinate.LEVEL_BLOCK)));
 				
 				Color color;
 				if (height == NO_HEIGHT_INFORMATION) {
@@ -66,7 +62,7 @@ public class ImageHeightMap implements HeightMap {
 			}
 		}
 		
-		imageProvider.setImage(chunk, image);
+		imageProvider.setImage(chunkLocation, image);
 		
 	}
 	

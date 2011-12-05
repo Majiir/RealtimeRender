9690eaf2f441764cb79039e55c68f3fd12224a49
Majiir <henry@zephyrhosting.net>
2011-12-04 19:37:10 -0500

    Created HeightMapReadCache. Analogous to ImageReadCache.

---
+++ RealtimeRender/src/main/java/net/nabaal/majiir/realtimerender/rendering/HeightMapReadCache.java
@@ -0,0 +1,43 @@
+package net.nabaal.majiir.realtimerender.rendering;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import net.nabaal.majiir.realtimerender.image.Coordinate;
+
+public class HeightMapReadCache extends HeightMapChunkProvider {
+
+	private final ConcurrentMap<Coordinate, HeightMapChunk> chunks = new ConcurrentHashMap<Coordinate, HeightMapChunk>();
+	private final HeightMapChunkProvider source;
+	
+	public HeightMapReadCache(HeightMapChunkProvider source) {
+		this.source = source;
+	}
+	
+	public void clear() {
+		chunks.clear();
+	}
+
+	@Override
+	protected HeightMapChunk getHeightMapChunk(Coordinate chunkLocation) {
+		if (chunks.containsKey(chunkLocation)) {
+			return chunks.get(chunkLocation);
+		}
+		HeightMapChunk chunk = source.getHeightMapChunk(chunkLocation);
+		if (chunk != null) {
+			chunks.put(chunkLocation, chunk);
+		}
+		return chunk;
+	}
+
+	@Override
+	protected void setHeightMapChunk(Coordinate chunkLocation, HeightMapChunk chunk) {
+		if (chunk != null) {
+			chunks.put(chunkLocation, chunk);
+		} else {
+			chunks.remove(chunkLocation);
+		}
+		source.setHeightMapChunk(chunkLocation, chunk);
+	}
+
+}

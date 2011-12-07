b06e8c53816083310276cb41abb63bee3ed860d0
Majiir Paktu <majiir@nabaal.net>
2011-12-06 23:10:13 -0500

    Multithreaded chunk rendering for all stages. Note that image processing is still single-threaded, however it is performed asynchronously.

--- src/main/java/net/nabaal/majiir/realtimerender/rendering/ChunkManager.java
+++ src/main/java/net/nabaal/majiir/realtimerender/rendering/ChunkManager.java
@@ -1,12 +1,18 @@
 package net.nabaal.majiir.realtimerender.rendering;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import org.bukkit.ChunkSnapshot;
 
 import net.nabaal.majiir.realtimerender.Coordinate;
+import net.nabaal.majiir.realtimerender.RealtimeRender;
 import net.nabaal.majiir.realtimerender.RenderChunkTask;
 import net.nabaal.majiir.realtimerender.image.ChunkRenderer;
 
@@ -28,9 +34,19 @@ public class ChunkManager {
 	}
 	
 	public void render(ChunkRenderer chunkRenderer) {
-		// TODO: Multithreading
+		ExecutorService executor = RealtimeRender.getExecutor();
+		List<Future<?>> futures = new ArrayList<Future<?>>();
 		for (ChunkSnapshot snapshot : snapshots.values()) {
-			new RenderChunkTask(chunkRenderer, snapshot).run();
+			futures.add(executor.submit(new RenderChunkTask(chunkRenderer, snapshot)));
+		}
+		for (Future<?> future : futures) {
+			try {
+				future.get();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	

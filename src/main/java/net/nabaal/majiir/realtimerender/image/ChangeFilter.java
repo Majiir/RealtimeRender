03e9829f5d3fd100b2190b5cc666a00f7894e038
Majiir Paktu <majiir@nabaal.net>
2011-12-22 01:25:13 -0500

    Cleaned up ChangeFilter with a more robust comparison implementation

--- src/main/java/net/nabaal/majiir/realtimerender/image/ChangeFilter.java
+++ src/main/java/net/nabaal/majiir/realtimerender/image/ChangeFilter.java
@@ -21,41 +21,24 @@ public class ChangeFilter extends ImageProvider {
 
 	@Override
 	public void setImage(Coordinate tile, BufferedImage image) {
-		
-		boolean changed = false;
-		
-		BufferedImage original = source.getImage(tile);
-		
-		if (original != null) {
-			
-			int width = original.getWidth();
-			int height = original.getHeight();
-			
-			if (width != image.getWidth()) {
-				changed = true;
-			}
-			
-			if (!changed && (height != image.getHeight())) {
-				changed = true;
-			}
-			
-			for (int x = 0; x < width; x++) {
-				for (int y = 0; (y < height) && !changed; y++) {
-					int oldPixel = original.getRGB(x, y);
-					int newPixel = image.getRGB(x, y);
-					if (oldPixel != newPixel) {
-						changed = true;
-					}
-				}
-			}
-			
-		} else {
-			changed = true; // TODO: Clean this whole routine up
-		}
-		
-		if (changed) {
+		if (!imageCompare(image, source.getImage(tile))) {
 			source.setImage(tile, image);
 		}
+	}
+	
+	private static boolean imageCompare(BufferedImage a, BufferedImage b) {
+		if (a == b) { return true; }
+		if ((a == null) || (b == null)) { return false; }
+		if (a.getWidth() != b.getWidth()) { return false; }
+		if (a.getHeight() != b.getHeight()) { return false; }
+		for (int x = 0; x < a.getWidth(); x++) {
+			for (int y = 0; y < a.getHeight(); y++) {
+				if (a.getRGB(x, y) != b.getRGB(x, y)) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	@Override

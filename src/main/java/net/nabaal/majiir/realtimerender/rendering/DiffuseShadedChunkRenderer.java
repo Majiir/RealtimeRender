8bbc97cee0379ba5f511df3aef5b85120065edf1
Majiir Paktu <majiir@nabaal.net>
2012-02-01 19:13:28 -0500

    Revert "Clearer deep water"
    This reverts commit f192cf672315028812d2607a163674f7ef695f1b.

--- src/main/java/net/nabaal/majiir/realtimerender/rendering/DiffuseShadedChunkRenderer.java
+++ src/main/java/net/nabaal/majiir/realtimerender/rendering/DiffuseShadedChunkRenderer.java
@@ -60,7 +60,7 @@ public class DiffuseShadedChunkRenderer implements ChunkRenderer {
 			Color color = materialColor.getColor(chunkSnapshot.getBlockData(x, y, z), x + (chunkSnapshot.getX() * 16), z + (chunkSnapshot.getZ() * 16), chunkSnapshot.getRawBiomeRainfall(x, z), chunkSnapshot.getRawBiomeTemperature(x, z), chunkSnapshot.getBiome(x, z));
 			if (color != null) {
 				if (material.equals(Material.STATIONARY_WATER) && isUnderWater(chunkSnapshot, x, y, z)) {
-					color = setAlpha(color, 24);
+					color = setAlpha(color, 32);
 				}
 				return color;
 			}
